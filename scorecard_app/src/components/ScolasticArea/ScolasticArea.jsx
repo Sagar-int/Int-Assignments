44da8619387962c5d9fe6f009046805dbe49e3b9
Sagar-int <sagar.kurewar@indusnet.co.in>
2022-08-11 11:18:14 +0530

    update

--- scorecard_app/src/components/ScolasticArea/ScolasticArea.jsx
+++ scorecard_app/src/components/ScolasticArea/ScolasticArea.jsx
@@ -4,29 +4,7 @@ import "./scolasticArea.css";
 export const ScolasticArea = () => {
   return (
     <Table striped bordered hover size="sm">
-      {/* <thead>
-        <tr>
-          <th rowSpan={2}>Sr. No</th>
-          <th rowSpan={2}>SUBJECTS</th>
-          <th colSpan={2}>
-            <tr className="test_row">
-              <th>A</th>
-              <th>B</th>
-              <th>C</th>
-              <th>D</th>
-              <th>E</th>
-            </tr>
-            <tr>
-              <th>a</th>
-              <th>b</th>
-              <th>c</th>
-              <th>d</th>
-              <th>e</th>
-            </tr>
-          </th>
-        </tr>
-      </thead> */}
-
+      
       <thead>
         <tr>
           <th className="table_head" rowspan={2}>
