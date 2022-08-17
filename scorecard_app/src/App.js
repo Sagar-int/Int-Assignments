b66b7219107dfe542e784fb91bbd804b9e99cc67
Sagar-int <sagar.kurewar@indusnet.co.in>
2022-08-17 18:29:51 +0530

    input data form start

--- scorecard_app/src/App.js
+++ scorecard_app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { MainForm } from './components/forms/mainform/MainForm';
 import { HomePage } from './components/homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <MainForm />
     </div>
   );
 }
